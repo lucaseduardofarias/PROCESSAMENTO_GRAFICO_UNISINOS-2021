db70d86875e8d770fbb39516d67085d08ece0b55
Lucas Eduardo <lucas.farias@multi24h.com.br>
2021-05-24 20:11:32 -0300

    Atualização LISTA 5

--- LISTA5/Ex07/Source.cpp
+++ LISTA5/Ex07/Source.cpp
@@ -1,11 +1,4 @@
-/* Hello Transforms - código inicial fornecido em https://learnopengl.com/#!Getting-started/Hello-Triangle 
- *
- * Adaptado por Rossana Baptista Queiroz
- * para a disciplina de Computação Gráfica - Jogos Digitais - Unisinos
- * Versão inicial: 7/4/2017
- * Última atualização em 14/03/2018
- *
- */
+
 
 using namespace std;
 
